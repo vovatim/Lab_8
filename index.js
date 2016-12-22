0ad628d63fc317a33a52de90cafe11bc45afede6
vovatim <vovatim1990@gmail.com>
2016-12-22 14:48:56 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ $('.plot').click(function (e) {
 
     console.log(poinst);
     console.log(fun);
-    if (i < x2)
-    {
-    graf=setInterval(function () {
-        $.plot($('.graph'), [{label: fun, data: poinst}], {});
-        x = x + (x2 - x1) / 100;
-        console.log(poinst);
-        if (poinst.length > 100) {
-            poinst.splice(1, 1)
+
+    graf = setInterval(function () {
+        if (x < x2) {
+            $.plot($('.graph'), [{label: fun, data: poinst}], {});
+            x = x + (x2 - x1) / 100;
+            console.log(poinst);
+            if (poinst.length > 100) {
+                poinst= poinst.splice(1)
+            }
+            poinst.push([x, eval(fun)])
+            i += parseFloat(step)
         }
-        poinst.push([x, eval(fun)])
+        else {
+            clearInterval(graf);
+        }
     }, 100);
-        i += parseFloat(step)
-    }
-    else
-    {
-        clearInterval(graf);
-    }
+
 });
